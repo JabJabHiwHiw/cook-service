7ead90d24301969a25765293a1fd92da4ded6d29
nicepunn <punnawat58000@gmail.com>
2024-11-12 06:16:36 +0700

    fix all

--- internal/models/cook.go
+++ internal/models/cook.go
@@ -2,24 +2,8 @@ package models
 
 import "github.com/google/uuid"
 
-type Cook struct {
-	ID             uuid.UUID `json:"id"`
-	Name           string    `json:"name"`
-	Email          string    `json:"email"`
-	ClerkId        string    `json:"clerk_id,omitempty"`
-	ProfilePicture string    `json:"profile_picture"`
-}
-
 type FavoriteMenu struct {
 	ID     uuid.UUID `json:"id"`
 	UserID uuid.UUID `json:"user_id,omitempty"`
 	MenuID uuid.UUID `json:"menu_id"`
-}
-
-type Profile struct {
-	ID             uuid.UUID `json:"id,omitempty"`
-	Name           string    `json:"name"`
-	Email          string    `json:"email"`
-	ClerkId        string    `json:"clerk_id,omitempty"`
-	ProfilePicture string    `json:"profile_picture"`
 }
